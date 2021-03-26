d0c1aed5551c1d2fe8c225e6578e203958f4bcfc
geethubacc <58160931+geethubacc@users.noreply.github.com>
2021-03-26 15:31:53 +0800

    Add files via upload

---
+++ test_2/test2javanfc/src/empTable.java
@@ -0,0 +1,191 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.sql.*;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javax.swing.JOptionPane;
+
+public class empTable implements Initializable {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane pane_EmpDB;
+
+    @FXML
+    private Button btn_Add;
+
+    @FXML
+    private Button btn_Update;
+
+    @FXML
+    private Button btn_Back;
+
+    @FXML
+    private Button btn_Delete;
+
+    @FXML
+    private TextField filterField;
+
+    @FXML
+    private TableView<empDetails> table_EmpDB;
+
+    @FXML
+    private TableColumn<empDetails, Integer> col_EmpID;
+
+    @FXML
+    private TableColumn<empDetails, String> col_EmpName;
+
+    @FXML
+    private TableColumn<empDetails, String> col_EmpSerNum;
+
+    ObservableList<empDetails> listM;
+    ObservableList<empDetails> dataList;
+
+    int index = -1;
+
+    Connection conn = null;
+    ResultSet rs = null;
+    PreparedStatement pstmt = null;
+
+    @FXML
+    void update_Table() {
+        col_EmpID.setCellValueFactory(new PropertyValueFactory<empDetails, Integer>("id"));
+        col_EmpName.setCellValueFactory(new PropertyValueFactory<empDetails, String>("name"));
+        col_EmpSerNum.setCellValueFactory(new PropertyValueFactory<empDetails, String>("serNum"));
+        listM = mysqlconnect.getEmpData();
+        table_EmpDB.setItems(listM);
+    }
+
+    @FXML
+    void add_Emp(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("empAdd.fxml"));
+            AnchorPane pane = loader.load();
+            pane_EmpDB.getChildren().setAll(pane);
+            empAdd obj = loader.getController();
+            obj.setButtonID(btn_Add.getId());
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+
+    @FXML
+    void getSelected(MouseEvent event) {
+        index = table_EmpDB.getSelectionModel().getSelectedIndex();
+        if (index <= -1) {
+            return;
+        }
+    }
+
+    @FXML
+    void edit_Selected() {
+        index = table_EmpDB.getSelectionModel().getSelectedIndex();
+        if (index <= -1) {
+            return;
+        }
+        String edit_EmpID = col_EmpID.getCellData(index).toString();
+        String edit_EmpName = col_EmpName.getCellData(index).toString();
+        String edit_EmpSerNum = col_EmpSerNum.getCellData(index).toString();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("empAdd.fxml"));
+            AnchorPane pane = loader.load();
+            pane_EmpDB.getChildren().setAll(pane);
+            empAdd obj = loader.getController();
+            obj.setButtonID(btn_Update.getId());
+            obj.setEmpID(edit_EmpID);
+            obj.setEmpName(edit_EmpName);
+            obj.setEmpSerNum(edit_EmpSerNum);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+
+    @FXML
+    void btn_Back(ActionEvent event) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("choice.fxml"));
+            pane_EmpDB.getChildren().setAll(pane);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+
+    @FXML
+    void delete_Selected(ActionEvent event) {
+        index = table_EmpDB.getSelectionModel().getSelectedIndex();
+        if (index <= -1) {
+            return;
+        }
+        conn = mysqlconnect.ConnectDb();
+        //String sql1 = "delete from attendance where emp_id = ?";
+        String sql2 = "delete from employee where emp_id = ?";
+        String del_ID = col_EmpID.getCellData(index).toString();
+        try {
+            //pstmt = conn.prepareStatement(sql1);
+            //pstmt.setString(1, del_ID);
+            //pstmt.execute();
+            pstmt = conn.prepareStatement(sql2);
+            pstmt.setString(1, del_ID);
+            pstmt.execute();
+            update_Table();
+            search_Table();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+
+    @FXML
+    void search_Table() {          
+        col_EmpID.setCellValueFactory(new PropertyValueFactory<empDetails, Integer>("id"));
+        col_EmpName.setCellValueFactory(new PropertyValueFactory<empDetails, String>("name"));
+        col_EmpSerNum.setCellValueFactory(new PropertyValueFactory<empDetails, String>("serNum"));
+        
+        dataList = mysqlconnect.getEmpData();
+        table_EmpDB.setItems(dataList);
+        FilteredList<empDetails> filteredData = new FilteredList<>(dataList, b -> true);  
+        filterField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(person -> {
+                if (newValue == null || newValue.isEmpty()) {
+                return true;
+                }    
+                String lowerCaseFilter = newValue.toLowerCase();
+                
+                if (person.getName().toLowerCase().indexOf(lowerCaseFilter) != -1 ) {
+                    return true;
+                } 
+                else if (person.getSerNum().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                    return true;
+                }                       
+                else {
+                    return false;
+                }
+            });
+        });  
+        SortedList<empDetails> sortedData = new SortedList<>(filteredData);  
+        sortedData.comparatorProperty().bind(table_EmpDB.comparatorProperty());  
+        table_EmpDB.setItems(sortedData);      
+    }
+
+    @FXML
+    public void initialize(URL url, ResourceBundle rb) {
+        update_Table();
+        search_Table();
+    }
+}
---
+++ test_2/test2javanfc/src/mysqlconnect.java
@@ -0,0 +1,58 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.*;
+import javax.swing.JOptionPane;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class mysqlconnect {
+    Connection conn = null;
+    public static Connection ConnectDb(){
+        try {
+            String url = "jdbc:mysql://localhost:3306/emp_db";
+            String hostUser = "root";
+            String hostPword = "HidayatJ48";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, hostUser, hostPword);
+            return conn;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        } 
+    }
+
+    public static ObservableList<empDetails> getEmpData() {
+        Connection conn = ConnectDb();
+        ObservableList<empDetails> list = FXCollections.observableArrayList();
+        try {
+            String sql = "select * from employee";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                list.add(new empDetails(Integer.parseInt(rs.getString("emp_id")), rs.getString("emp_name"), rs.getString("nfc_num")));
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return list;
+    }
+
+    public static ObservableList<attDetails> getAttData() {
+        Connection conn = ConnectDb();
+        ObservableList<attDetails> list = FXCollections.observableArrayList();
+        try {
+            String sql = "select * from attendance";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                list.add(new attDetails(Integer.parseInt(rs.getString("att_id")), rs.getString("clockin_date"), rs.getString("clockin_time"), rs.getString("clockout_date"), rs.getString("clockout_time"), Integer.parseInt(rs.getString("isLate")), Integer.parseInt(rs.getString("emp_id"))));
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return list;
+    }
+    
+}
